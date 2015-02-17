3c67ba7b40410a4472d7588c671edba4d8a8beab
Torsten Werner <tw@twerner.eu>
2015-02-17 15:58:11 +0100

    fixed warnings

--- sockets/src/main/java/dhbw/verteilteSysteme/sockets/App.java
+++ sockets/src/main/java/dhbw/verteilteSysteme/sockets/App.java
@@ -10,39 +10,38 @@ import java.util.Scanner;
  * Hello world!
  *
  */
-public class App 
-{
-	private static void handleConnection(Socket client) throws IOException{
-    	Scanner in = new Scanner (client.getInputStream());
-    	PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-    	
-    	String input = in.nextLine();
-    	
-    	out.println(String.format("You entered: %s", input));
-    }
-    public static void main( String[] args ) throws IOException
-    {
-        ServerSocket server = new ServerSocket(4321);
-        
-        while(true){
-        	Socket client = null;
-        	
-        	try{
-        		client = server.accept();
-        		handleConnection(client);
-        	} catch (IOException e){
-        		e.printStackTrace();
-        	} finally{
-        		if (client != null){
-        			try{
-        				client.close();
-        			}catch (IOException e){
-        				
-        			}
-        		}
-        	}
-        }
-        
-    }
-    
+public class App {
+	public static void main(String[] args) throws IOException {
+		ServerSocket server = new ServerSocket(4321);
+
+		while (true) {
+			Socket client = null;
+
+			try {
+				client = server.accept();
+				handleConnection(client);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (client != null) {
+					try {
+						client.close();
+					} catch (IOException e) {
+
+					}
+				}
+			}
+		}
+	}
+
+	private static void handleConnection(Socket client) throws IOException {
+		Scanner in = new Scanner(client.getInputStream());
+		PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+
+		String input = in.nextLine();
+
+		out.println(String.format("You entered: %s", input));
+		in.close();
+	}
+
 }
